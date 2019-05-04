897d56c0781c7249bd012b8db0934374241ca36e
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-04 15:14:52 -0500

    echo hello

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node{
-    properties([parameters([string(defaultValue: 'IP', description: 'Where i build e.g IP', name: "ENV", trim: true)])])
+    properties([parameters([string(defaultValue: 'IP', description: 'Where i build e.g IP', name: 'ENV', trim: true)])])
     stage("Clone repo"){
         git 'https://github.com/farrukh90/Flaskex.git'
     }
