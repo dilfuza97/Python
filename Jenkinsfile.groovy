d3c986299554c497f7af577aa606e3fec642b0c0
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-05-04 15:19:39 -0500

    change

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ node{
     }
     stage("Build application"){
         sh   "scp -r *   ec2-user@${ENV}:/tmp"
-        sh "ssh ec2-user@${ENV} sudo   yum install python -pip -y"
+        sh "ssh ec2-user@${ENV} sudo   yum install python-pip -y"
         sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
     } 
     stage("App Run"){
