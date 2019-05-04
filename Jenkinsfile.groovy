9d669e16cef3717c393ac1ac81c5218d0b2a81b7
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-05-04 16:12:25 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,13 @@
 node{
-    properties([parameters([string(defaultValue: 'IP', description: 'Where i build e.g IP', name: 'ENV', trim: true)])])
+    properties([parameters([string(defaultValue: 'IP', description: 'Where to build e.g IP', name: 'ENV', trim: true)])])
     stage("Clone repo"){
-        git 'https://github.com/farrukh90/Flaskex.git'
+        git 'git@github.com:farrukh90/stormpath-flask-sample.git'
     }
     stage("Build application"){
-        sh   "scp -r *   ec2-user@${ENV}:/tmp"
-        sh "ssh ec2-user@${ENV} sudo   yum install python-pip -y"
+        sh "scp -r * ec2-user@${ENV}:/tmp"
         sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
-    } 
-    stage("App Run"){
-        sh "ssh ec2-user@${ENV} python /tmp/app.py"
     }
-    stage("Test app"){
-        sh "selenium"
-    }            
-
-    
-
-}           
+    stage("App Run"){
+        sh "ssh ec2-user@${ENV}  python  /tmp/app.py"
+    }
+}
